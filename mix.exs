85fb8c2697d553c3bf1bb91ba874c64d3cb5ff65
Blake Kostner <git@btkostner.io>
2020-02-15 22:40:33 -0700

    add credo to github actions

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule FlatpakAuth.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.2", only: [:dev], runtime: false},
       {:phoenix, "~> 1.4.13"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
