3329857b88e2c71a7a308f617d4632cd31f4c69d
Blake Kostner <git@btkostner.io>
2020-03-13 22:57:46 -0600

    load ssl lib for postgresql

--- lib/accounts/release.ex
+++ lib/accounts/release.ex
@@ -8,20 +8,29 @@ defmodule Accounts.Release do
   ```
   """
 
-  @app :accounts
+  @apps [:accounts, :ssl]
 
   def migrate do
+    load()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    load()
+
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  defp load do
+    for app <- @apps do
+      Application.load(app)
+    end
+  end
+
   defp repos do
-    Application.load(@app)
-    Application.fetch_env!(@app, :ecto_repos)
+    Application.fetch_env!(:accounts, :ecto_repos)
   end
 end
